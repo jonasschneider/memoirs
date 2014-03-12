bf6aa7e513cce53358bfb6015294a0aed8fa7ca7
Jonas Schneider <mail@jonasschneider.com>
2014-03-12 19:08:11 +0100

    allow idiot-proof quotes formatted as single-line dialogues

--- lib/memoir.rb
+++ lib/memoir.rb
@@ -22,19 +22,34 @@ class Memoir
   QUOTE_EX = /^"(.*)"(?: - (.*))?$/m
 
   def is_quote?
+    return true if defacto_quote?
     body && body.match(QUOTE_EX)
   end
 
   def quoted_text
+    return defacto_quote_text if defacto_quote?
     $1.gsub("\n", "<br />") if body.match(QUOTE_EX)
   end
 
   def quote_source
+    return defacto_quote_source if defacto_quote?
     $2 if body.match(QUOTE_EX)
   end
 
   def is_dialogue?
     body && !!body.match(/\[(.*)\]/)
+  end
+
+  def defacto_quote?
+    dialogue_lines.length == 1 && dialogue_lines.first[:speaker] && !dialogue_lines.first[:style]
+  end
+
+  def defacto_quote_text
+    dialogue_lines.first[:message]
+  end
+
+  def defacto_quote_source
+    dialogue_lines.first[:speaker]
   end
 
   def dialogue_lines
